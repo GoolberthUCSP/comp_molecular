dd92a5a1c0bc4268d4bcdbdecab9a2605ffa13c2
Fredy Goolberth Quispe Neira <fredy.quispe@ucsp.edu.pe>
2024-08-30 18:34:34 -0500

    Update

--- exercises/pairwise_alignment.cpp
+++ exercises/pairwise_alignment.cpp
@@ -1,65 +1,69 @@
-#include <iostream>
-#include <list>
-#include <utility>
+#include<iostream>
+#include<utility>
+#include<string>
 
 using namespace std;
 
-pair<string, string> pairwise_alignment(string a, string b) {
-    int n = a.size();
-    int m = b.size();
-    int **dp = new int *[n + 1];
-    for (int i = 0; i < n + 1; i++) {
-        dp[i] = new int[m + 1]{0};
-    }
+// Needelman-Wunsch Algorithm
+pair<string, string> pairwise_alignement(string s1, string s2){
+  int m = s1.size();
+  int n = s2.size();
+  pair<string, string> result;
+  int **dp = new int*[m+1];
+  for(int i = 0; i < m+1; i++){
+    dp[i] = new int[n+1];
+  }
 
-    for (int i = 1; i < n + 1; i++) {
-        for (int j = 1; j < m + 1; j++) {
-            if (a[i - 1] == b[j - 1]) {
-                dp[i][j] = dp[i - 1][j - 1] + 1;
-            } else {
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-            }
-        }
+  for(int i = 0; i < m+1; i++){
+    for(int j = 0; j < n+1; j++){
+      if (i == 0) dp[i][j] = j*(-2);
+      else if (j == 0) dp[i][j] = i*(-2);
+      else{
+        dp[i][j] = max(dp[i-1][j] - 2, 
+                   max(dp[i][j-1] - 2, 
+                   dp[i-1][j-1] + (s1[i-1] == s2[j-1] ? 1 : -1)));
+      }
     }
-    
-    int i = n;
-    int j = m;
-    string x = "";
-    string y = "";
-    while (i > 0 && j > 0) {
-        if (a[i - 1] == b[j - 1]) {
-            x = a[i - 1] + x;
-            y = b[j - 1] + y;
-            i--;
-            j--;
-        } else if (dp[i - 1][j] > dp[i][j - 1]) {
-            x = a[i - 1] + x;
-            y = "-" + y;
-            i--;
-        } else {
-            x = "-" + x;
-            y = b[j - 1] + y;
-            j--;
-        }
+  }
+
+  int i = m, j = n;
+  while(i > 0 && j > 0){
+    if (dp[i][j] == dp[i-1][j-1] + (s1[i-1] == s2[j-1] ? 1 : -1)){
+      result.first += s1[i-1];
+      result.second += s2[j-1];
+      i--;
+      j--;
+    } else if (dp[i][j] == dp[i-1][j] - 2){
+      result.first += s1[i-1];
+      result.second += "-";
+      i--;
+    } else if (dp[i][j] == dp[i][j-1] - 2){
+      result.first += "-";
+      result.second += s2[j-1];
+      j--;
     }
-    while (i > 0) {
-        x = a[i - 1] + x;
-        y = "-" + y;
-        i--;
-    }
-    while (j > 0) {
-        x = "-" + x;
-        y = b[j - 1] + y;
-        j--;
-    }
-    return make_pair(x, y);
+  }
+
+  while(i > 0){
+    result.first += s1[i-1];
+    result.second += "-";
+    i--;
+  }
+
+  while(j > 0){
+    result.first += "-";
+    result.second += s2[j-1];
+    j--;
+  }
+  result.first = string(result.first.rbegin(), result.first.rend());
+  result.second = string(result.second.rbegin(), result.second.rend());
+
+  return result;
 }
 
-int main() {
-    string a= "AGGTAB";
-    string b= "GXTXAYB";
-    pair<string, string> result = pairwise_alignment(a, b);
-    cout << result.first << endl;
-    cout << result.second << endl;
-    return 0;
+int main(){
+  pair<string, string> result = pairwise_alignement("AAAC", "AGC");
+  cout << result.first << endl;
+  cout << result.second << endl;
+  return 0;
 }
